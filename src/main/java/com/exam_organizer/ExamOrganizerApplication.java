1ac4973666bf376789ecf262cf126777970be452
amrendra03 <amrendra9076893685@gmail.com>
2023-10-27 20:00:11 +0530

    first commit

---
+++ src/main/java/com/exam_organizer/ExamOrganizerApplication.java
@@ -0,0 +1,14 @@
+package com.exam_organizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamOrganizerApplication.class, args);
+		System.out.println("Amrendra from intellije");
+	}
+
+}
---
+++ src/test/java/com/exam_organizer/ExamOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam_organizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
