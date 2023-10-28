babab1134c4b9de4369d0e64777f70f7cd62cf08
amrendra03 <amrendra9076893685@gmail.com>
2023-10-28 22:44:04 +0530

    28/10/2023-testing-resource-static-files

---
+++ src/main/resources/static/js/exam.js
@@ -0,0 +1,22 @@
+console.log("hello exam");
+alert("hello");
+
+function alertCopy() {
+    alert('Copy Successfully.');
+};
+
+function openPopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "block";
+
+}
+
+function closePopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+    console.log(flag);
+}
+
+
+
+
